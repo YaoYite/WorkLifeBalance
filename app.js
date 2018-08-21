593950a663de4b1addd2105a228b000fa185113e
ProblemSolvers <superyaoyite@gmail.com>
2018-08-21 15:22:01 +0000

    set up initial routes and index page

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index.ejs");
+});
+
+app.get("/about", function(req, res){
+    res.render("about.ejs");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact.ejs");
+});
+
+app.get("/workaholic", function(req, res){
+    res.render("workaholic.ejs");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The Work Life Balance Server Has Started!");
+});
