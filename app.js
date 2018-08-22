1190464ba1da1002b33fd91584006faa9ca0dad0
ProblemSolvers <superyaoyite@gmail.com>
2018-08-22 03:54:12 +0000

    apply bootstrap template

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+app.use( express.static( "public" ) );
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
