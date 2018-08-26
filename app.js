cdf46ac065f4fdbd663043026f43722b823ac6d7
ProblemSolvers <superyaoyite@gmail.com>
2018-08-26 10:09:28 +0000

    add some new subpages and update the about pages

--- app.js
+++ app.js
@@ -19,12 +19,36 @@ app.get("/contact", function(req, res){
     res.render("contact.ejs");
 });
 
-app.get("/workaholic", function(req, res){
-    res.render("workaholic.ejs");
+app.get("/assessment", function(req, res){
+    res.render("assessment.ejs");
 });
 
 app.get("/family", function(req, res){
     res.render("family.ejs");
+});
+
+app.get("/whyus", function(req, res){
+    res.render("whyus.ejs");
+});
+
+app.get("/important", function(req, res){
+    res.render("important.ejs");
+});
+
+app.get("/whatisworkaholic", function(req, res){
+    res.render("whatisworkaholic.ejs");
+});
+
+app.get("/difference", function(req, res){
+    res.render("difference.ejs");
+});
+
+app.get("/relatetomentalhealth", function(req, res){
+    res.render("relatetomentalhealth.ejs");
+});
+
+app.get("/workaholic", function(req, res){
+    res.render("workaholic.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
