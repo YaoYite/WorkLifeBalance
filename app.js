ed4ee7e58d196cee692147e86bbb9a1f0684b482
ProblemSolvers <superyaoyite@gmail.com>
2018-08-29 09:18:11 +0000

    add comments

--- app.js
+++ app.js
@@ -7,26 +7,32 @@ app.use( express.static( "public" ) );
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//Index page - landing page
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
 
+//About page - website and team information
 app.get("/about", function(req, res){
     res.render("about.ejs");
 });
 
+//Contact page - team contact information
 app.get("/contact", function(req, res){
     res.render("contact.ejs");
 });
 
+//Assessment page - workaholic quick assessment
 app.get("/assessment", function(req, res){
     res.render("assessment.ejs");
 });
 
+//Family page - Show general tips of how to help workaholic family
 app.get("/family", function(req, res){
     res.render("family.ejs");
 });
 
+//Workaholic page - show information of workaholic
 app.get("/workaholic", function(req, res){
     res.render("workaholic.ejs");
 });
