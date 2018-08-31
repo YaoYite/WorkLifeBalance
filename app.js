f4e29c8091563301d04851366e9375693b6bcf3d
ProblemSolvers <superyaoyite@gmail.com>
2018-08-31 06:40:03 +0000

    add authentication function

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var express = require("express");
 var app = express();
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var User = require("./models/user");
+
 app.use( express.static( "public" ) );
 
-
+mongoose.connect("mongodb://localhost/auth_demo_app",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "abcd1234",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=======================
+//Routes
+//=======================
+
+
 
 //Index page - landing page
 app.get("/", function(req, res){
@@ -36,6 +61,56 @@ app.get("/family", function(req, res){
 app.get("/workaholic", function(req, res){
     res.render("workaholic.ejs");
 });
+
+app.get("/secret",isLoggedIn,function(req, res){
+    res.render("secret.ejs");
+});
+
+//show sigu up form
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret");
+        });
+    });
+});
+
+//show login form
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+});
+
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Work Life Balance Server Has Started!");
